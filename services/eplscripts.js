776610f0970545b7f2a2abd9868db5d66959a901
Andrew Nisbet <andrew@dev-ils.com>
2021-06-28 13:40:55 -0400

    Added getuserpin as for testing on test ILS.

--- services/eplscripts.js
+++ services/eplscripts.js
@@ -6,7 +6,8 @@ const { spawnSync } = require( 'child_process' );
 const eplScripts = {};
 
 eplScripts.getUserPin = function(userId){
-    let cmd = spawnSync( 'ls', [ '-lh', '/usr' ] );
+    let cmd = spawnSync( 'getuserpin', [] );
+    // let cmd = spawnSync( 'ls', [ '-lh', '/usr' ] );
     // const cmd = spawnSync( 'sleep', [ '5' ] );
     return {"stdout":cmd.stdout.toString().split(/\r?\n/),"stderr":cmd.stderr.toString().split(/\r?\n/)};
 }
