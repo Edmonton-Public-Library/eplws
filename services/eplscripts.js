6bb67f240e856f153b2462c04ac03f0c5a5b1bf1
Andrew Nisbet <andrew@dev-ils.com>
2021-07-28 10:29:02 -0400

    Change status message to use serverstatus.sh to report more useful information about the server.

--- services/eplscripts.js
+++ services/eplscripts.js
@@ -35,4 +35,19 @@ eplScripts.getUserPin = function(userId){
 //     return {"stdout":cmd.stdout.toString().split(/\r?\n/),"stderr":cmd.stderr.toString().split(/\r?\n/)};
 // }
 
+/**
+ * Returns the pin of a given user by user ID.
+ * @param {*} userId 
+ * @returns 
+ */
+ eplScripts.getStatus = function(){
+    // let cmd = spawnSync( 'getuserpin', [`${userId}`] );
+    let cmd = spawnSync( 'serverstatus.sh', [] );
+    let out = cmd.stdout.toString().split(/\r?\n/);
+    out = utilityTests.filterEmptyStrings(out);
+    let err = cmd.stderr.toString().split(/\r?\n/);
+    err = utilityTests.filterEmptyStrings(err);
+    return {"stdout":out,"stderr":err};
+}
+
 module.exports = eplScripts;
